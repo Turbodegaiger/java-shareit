49f51c02be416ce3c861482700f6021324befb09
DmitryPrivalov <apothecary1@yandex.ru>
2023-08-13 15:15:30 +0300

    feat: устраняем ошибки тестов github 5

--- src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
@@ -1,12 +1,10 @@
 package ru.practicum.shareit.booking.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.enums.BookingStatus;
 import ru.practicum.shareit.booking.model.Booking;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     Iterable<Booking> findAllByBookerIdEqualsOrderByStartDesc(long userId);
